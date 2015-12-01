e6a1fdd3433822bf740506caae6b3c53e93c8ae1
Thomas Huzij <tph2109@columbia.edu>
2015-12-01 18:58:47 -0500

    allow uploading proguard mapping and clean up code style

--- src/main/groovy/com/nimbledroid/ProfilerPlugin.groovy
+++ src/main/groovy/com/nimbledroid/ProfilerPlugin.groovy
@@ -17,6 +17,7 @@ class ProfilerPluginExtension {
     String apiKey = null
     String variant = 'release'
     String apkFilename = null
+    String apkMapping = null
     long ndGetProfileTimeout = 1800
     String server = 'https://www.nimbledroid.com'
 }
@@ -32,23 +33,24 @@ class ProfilerPlugin implements Plugin<Project> {
     String nimbleVersion = null
 
     void apply(Project project) {
-        project.extensions.create("nimbledroid", ProfilerPluginExtension)
-        project.nimbledroid.extensions.create("appData", AppDataExtension)
+        project.extensions.create('nimbledroid', ProfilerPluginExtension)
+        project.nimbledroid.extensions.create('appData', AppDataExtension)
 
-        nimbleProperties = project.file("${project.rootDir}/nimbledroid.properties")
+        nimbleProperties = project.file("$project.rootDir/nimbledroid.properties")
         nimbleVersion = '1.0.6'
 
         project.task('ndUpload') << {
             http = new HTTPBuilder(project.nimbledroid.server)
             checkKey(project)
-            http.auth.basic(project.nimbledroid.apiKey, "")
+            http.auth.basic(project.nimbledroid.apiKey, '')
             String apkPath = null
             File apk = null
-            if(project.nimbledroid.apkFilename) {
+            File mapping = null
+            if (project.nimbledroid.apkFilename) {
                 apk = project.file("build/outputs/apk/$project.nimbledroid.apkFilename")
-                if(!apk.exists()) {
+                if (!apk.exists()) {
                     apk = new File(project.nimbledroid.apkFilename)
-                    if(!apk.exists()) {
+                    if (!apk.exists()) {
                         println "Could not find ${apk.getAbsolutePath()}"
                         ndUploadFailure()
                     }
@@ -56,23 +58,31 @@ class ProfilerPlugin implements Plugin<Project> {
             } else if (project.hasProperty('android')) {
                 project.android.applicationVariants.all { variant ->
                     variant.outputs.each { output ->
-                        if(variant.name == project.nimbledroid.variant) {
+                        if (variant.name == project.nimbledroid.variant) {
                             apkPath = output.outputFile
+                            mapping = variant.mappingFile
                         }
                     }
                 }
-                if(apkPath == null) {
-                    println "No variant named ${project.nimbledroid.variant}"
+                if (apkPath == null) {
+                    println "No variant named $project.nimbledroid.variant"
                     ndUploadFailure()
                 }
                 apk = project.file(apkPath)
                 if (!apk.exists()) {
-                    println "No apk exists for variant ${project.nimbledroid.variant}"
+                    println "No apk exists for variant $project.nimbledroid.variant"
                     ndUploadFailure()
                 }
             } else {
                 println "Could not find android block. Please apply the plugin to your app's build.gradle or define an apkFilename"
                 ndUploadFailure()
+            }
+            if (project.nimbledroid.apkMapping) {
+                mapping = new File(project.nimbledroid.apkMapping)
+                if (!mapping.exists()) {
+                    println "Could not find ${mapping.getAbsolutePath()}"
+                    ndUploadFailure()
+                }
             }
             http.request(POST, JSON) { req ->
                 uri.path = '/api/v1/apks'
@@ -81,39 +91,47 @@ class ProfilerPlugin implements Plugin<Project> {
                 requestContentType = 'multipart/form-data'
                 MultipartEntity entity = new MultipartEntity()
                 entity.addPart('apk', new FileBody(apk))
+                if (mapping) {
+                    entity.addPart('mapping', new FileBody(mapping))
+                    entity.addPart('has_mapping', new StringBody('true'))
+                }
                 try {
-                    String commitHash = "git rev-parse HEAD".execute().text.trim()
-                    if(commitHash != "") {
+                    String commitHash = 'git rev-parse HEAD'.execute().text.trim()
+                    if (commitHash) {
                         entity.addPart('commit', new StringBody(commitHash));
                     }
-                } catch(IOException e) {}
-                if(project.hasProperty('branch')) {
-                    entity.addPart('branch', new StringBody("${project.branch}"));
+                } catch (IOException e) {}
+                if (project.hasProperty('branch') && project.branch) {
+                    entity.addPart('branch', new StringBody(project.branch));
                 }
-                if(project.hasProperty('flavor')) {
-                    entity.addPart('flavor', new StringBody("${project.flavor}"));
+                if (project.hasProperty('flavor') && project.flavor) {
+                    entity.addPart('flavor', new StringBody(project.flavor));
                 }
-                if(project.nimbledroid.hasProperty('appData')) {
-                    if (project.nimbledroid.appData.username != null) {
-                        entity.addPart('auto_login', new StringBody("true"));
-                        entity.addPart('username', new StringBody("${project.nimbledroid.appData.username}"));
-                        entity.addPart('password', new StringBody("${project.nimbledroid.appData.password}"));
+                if (project.nimbledroid.hasProperty('appData')) {
+                    if (project.nimbledroid.appData.username || project.nimbledroid.appData.password) {
+                        entity.addPart('auto_login', new StringBody('true'));
+                    }
+                    if (project.nimbledroid.appData.username) {
+                        entity.addPart('username', new StringBody(project.nimbledroid.appData.username));
+                    }
+                    if (project.nimbledroid.appData.password) {
+                        entity.addPart('password', new StringBody(project.nimbledroid.appData.password));
                     }
                 }
                 req.entity = entity
                 response.success = { resp, reader ->
-                    if(reader.console_message != null) {
+                    if (reader.console_message) {
                         println reader.console_message
                     }
-                    if(reader.profile_url != null) {
-                        println "Profile URL: ${reader.profile_url}"
+                    if (reader.profile_url) {
+                        println "Profile URL: $reader.profile_url"
                         nimbleProperties.write(reader.profile_url)
                     } else {
                         ndUploadFailure()
                     }
                 }
                 response.'401' = { resp ->
-                      println "Invalid API key, visit ${project.nimbledroid.server}/account to retrieve the current key."
+                      println "Invalid API key, visit $project.nimbledroid.server/account to retrieve the current key."
                       println 'You can contact support@nimbledroid.com if you need assistance.'
                 }
                 response.failure = { resp ->
@@ -127,27 +145,27 @@ class ProfilerPlugin implements Plugin<Project> {
         project.task('ndGetProfile') << {
             http = new HTTPBuilder(project.nimbledroid.server)
             checkKey(project)
-            if(!nimbleProperties.exists()) {
-                println 'Couldn\'t find nimbledroid.properties file, ndUpload task was either not run or failed.'
+            if (!nimbleProperties.exists()) {
+                println "Couldn\'t find nimbledroid.properties file, ndUpload task was either not run or failed."
                 throw new StopActionException()
             }
-            http.auth.basic(project.nimbledroid.apiKey, "")
+            http.auth.basic(project.nimbledroid.apiKey, '')
             Boolean done = false
             String latestProfile = new String(nimbleProperties.readBytes()).trim()
             String uriPath = "/api/v1$latestProfile"
             try {
                 URL url = new URL(latestProfile)
                 uriPath = url.getPath()
-            } catch(MalformedURLException e) {}
+            } catch (MalformedURLException e) {}
             long timeout = System.nanoTime() + TimeUnit.NANOSECONDS.convert(project.nimbledroid.ndGetProfileTimeout, TimeUnit.SECONDS)
-            while(!done) {
+            while (!done) {
                 http.request(GET) { req ->
                     uri.path = uriPath
                     headers.'User-Agent' = "NimbleDroid Profiler Gradle Plugin/$nimbleVersion"
                     headers.'gradle' = nimbleVersion
                     response.success = { resp, reader ->
-                        switch(reader.status) {
-                            case ["Profiled", "Failed"]:
+                        switch (reader.status) {
+                            case ['Profiled', 'Failed']:
                                 println reader.console_message
                                 done = true
                                 break
@@ -167,16 +185,16 @@ class ProfilerPlugin implements Plugin<Project> {
                         done = true
                     }
                     response.'401' = { resp ->
-                          println "Invalid API key, visit ${project.nimbledroid.server}/account to retrieve the current key."
+                          println "Invalid API key, visit $project.nimbledroid.server/account to retrieve the current key."
                           println 'You can contact support@nimbledroid.com if you need assistance.'
                           done = true
                     }
                 }
-                if(!done) {
+                if (!done) {
                     sleep(30000)
                 }
             }
-            if(nimbleProperties.exists()) {
+            if (nimbleProperties.exists()) {
                 nimbleProperties.delete()
             }
         }
@@ -184,7 +202,7 @@ class ProfilerPlugin implements Plugin<Project> {
         project.task('ndProfile') << {
             checkKey(project)
             project.ndUpload.execute()
-            if(nimbleProperties.exists()) {
+            if (nimbleProperties.exists()) {
                 project.ndGetProfile.execute()
             }
         }
@@ -198,7 +216,7 @@ class ProfilerPlugin implements Plugin<Project> {
     }
 
     void ndUploadFailure() {
-        if(nimbleProperties.exists()) {
+        if (nimbleProperties.exists()) {
             nimbleProperties.delete()
         }
         throw new StopActionException()
